a5dcd5ae98a082b8629879a06a5e5a121041a359
Auclown <auclown@gmail.com>
2021-04-30 21:37:16 +1200

    30th/Apr/2021 - main_drawer.dart added

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/main_drawer.dart';
 import './categories_screen.dart';
 import './favourites_screen.dart';
 
@@ -34,9 +35,7 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(_pages[_selectedPageIndex]['title']),
       ),
-      drawer: Drawer(
-        child: Text('The drawer'),
-      ),
+      drawer: MainDrawer(),
       body: _pages[_selectedPageIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
           onTap: _selectPage,
